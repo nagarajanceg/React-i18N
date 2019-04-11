04f7394e8d87914462787bd4a816d77541879498
nagarajanceg <nagharajang@gmail.com>
2019-04-11 08:57:28 -0700

    react i18n setup

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+// the hoc
+import { withNamespaces } from 'react-i18next';
+
+function App ({ t }) {
+  return <h1>{t('Welcome to React')}</h1>
 }
 
-export default App;
+export default withNamespaces()(App);
